7eef23c68961006bc9e42f21c825c79e7e3416b2
Oliver Stanley <o.dzineit@gmail.com>
2023-01-18 18:19:04 +0000

    Require IDs as inputs

--- scripts/input_matches.py
+++ scripts/input_matches.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 def main():
     matches = pd.read_csv("data/Matches.csv")
-    teams = pd.read_csv("data/Teams.csv")
 
     while True:
         print(f"Enter match [Team1 Team2 Date IndexInDate] (Type 'exit' to finish)")
@@ -13,9 +12,8 @@ def main():
             break
 
         try:
-            team1_name, team2_name, date, index_in_date = user_input.split(" ")
-            team1_id, team2_id = teams[teams.Name == team1_name].ID.iloc[0], teams[teams.Name == team2_name].ID.iloc[0]
-            index_in_date = int(index_in_date)
+            team1_id, team2_id, date, index_in_date = user_input.split(" ")
+            team1_id, team2_id, index_in_date = int(team1_id), int(team2_id), int(index_in_date)
             matches = pd.concat((matches, pd.DataFrame([[len(matches), date, index_in_date, team1_id, team2_id]], columns=matches.columns)))
         except Exception as e:
             print(e)
